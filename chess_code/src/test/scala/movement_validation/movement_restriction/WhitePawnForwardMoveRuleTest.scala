5b369a3bf073c6c819e3833ade5d50d51dd74003
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-10-09 18:03:45 -0300

    Rename single and double PawnMovement

--- chess_code/src/test/scala/movement_validation/movement_restriction/WhitePawnForwardMoveRuleTest.scala
+++ chess_code/src/test/scala/movement_validation/movement_restriction/WhitePawnForwardMoveRuleTest.scala
@@ -13,18 +13,18 @@ class WhitePawnForwardMoveRuleTest extends AnyFunSuite {
   val boardCoordinate: BoardCoordinate = BoardCoordinate(2,2)
   private val pawnType: PieceType = PieceType("PAWN")
   val whitePawn: Piece = Piece(whiteColor,pawnType)
-  val pawnSecondPositionBoard: SinglePieceBoard = SinglePieceBoard(boardCoordinate,whitePawn)
+  val initialCoordinateBoard: SinglePieceBoard = SinglePieceBoard(boardCoordinate,whitePawn)
   val pawnRules: MovementRestriction = PawnRule()
 
-  test("Single move forward test"){
+  test("Single move forward test initial position"){
     val movement = Movement(boardCoordinate,BoardCoordinate(2, 3))
-    val movementRestriction = MoveRestrictionInput(movement, pawnSecondPositionBoard,List())
+    val movementRestriction = MoveRestrictionInput(movement, initialCoordinateBoard,List())
     assert(pawnRules.checkIfRestrictionIsMet(movementRestriction))
   }
 
-  test("Double move forward test") {
+  test("Double move forward test initial position") {
     val movement = Movement(boardCoordinate, BoardCoordinate(2, 4))
-    val movementRestriction = MoveRestrictionInput(movement, pawnSecondPositionBoard, List())
+    val movementRestriction = MoveRestrictionInput(movement, initialCoordinateBoard, List())
     assert(pawnRules.checkIfRestrictionIsMet(movementRestriction))
   }
 
