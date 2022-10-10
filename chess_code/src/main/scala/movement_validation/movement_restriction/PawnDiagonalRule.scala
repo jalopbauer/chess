c432a70af7da36e8dc4de48863f2c43c3f94e593
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-10-10 16:39:02 -0300

    R diagonal PawnMovement

---
+++ chess_code/src/main/scala/movement_validation/movement_restriction/PawnDiagonalRule.scala
@@ -0,0 +1,18 @@
+package movement_validation.movement_restriction
+
+import board.BoardCoordinate
+import movement_validation.MoveRestrictionInput
+
+  case class PawnDiagonalRule() extends MovementRestriction:
+    def checkIfRestrictionIsMet(moveRestrictionInput: MoveRestrictionInput): Boolean =
+      val movement = moveRestrictionInput.movement
+      val from = movement.from
+      val to = movement.to
+      val vector = to.vector(from)
+      val maybeFromPiece = moveRestrictionInput.board.getPieceInBoardCoordinate(from)
+      val maybeToPiece = moveRestrictionInput.board.getPieceInBoardCoordinate(to)
+      val value = maybeFromPiece.get
+      val value1 = maybeToPiece.get
+      (vector == BoardCoordinate(1, 1)
+        || vector == BoardCoordinate(-1, 1))
+        && value.color != value1.color
