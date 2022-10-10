d4fa9bc1524bbedeaf3957cf264b4b3f3e14f85d
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-10-09 21:12:56 -0300

    Single and double PawnMovement redundancy tests

--- chess_code/src/test/scala/movement_validation/movement_restriction/WhitePawnForwardMoveRuleTest.scala
+++ chess_code/src/test/scala/movement_validation/movement_restriction/WhitePawnForwardMoveRuleTest.scala
@@ -29,6 +29,21 @@ class WhitePawnForwardMoveRuleTest extends AnyFunSuite {
     assert(pawnRules.checkIfRestrictionIsMet(movementRestriction))
   }
 
+  val initialBoardCoordinate2: BoardCoordinate = BoardCoordinate(3, 2)
+  val initialCoordinateBoard2: SinglePieceBoard = SinglePieceBoard(initialBoardCoordinate2, whitePawn)
+
+  test("Single move forward test initial position redundancy") {
+    val movement = Movement(initialBoardCoordinate2, BoardCoordinate(3, 3))
+    val movementRestriction = MoveRestrictionInput(movement, initialCoordinateBoard2, List())
+    assert(pawnRules.checkIfRestrictionIsMet(movementRestriction))
+  }
+
+  test("Double move forward test initial position redundancy") {
+    val movement = Movement(initialBoardCoordinate2, BoardCoordinate(3, 4))
+    val movementRestriction = MoveRestrictionInput(movement, initialCoordinateBoard2, List())
+    assert(pawnRules.checkIfRestrictionIsMet(movementRestriction))
+  }
+
   val movedBoardCoordinate: BoardCoordinate = BoardCoordinate(2, 3)
   val movedCoordinateBoard: SinglePieceBoard = SinglePieceBoard(movedBoardCoordinate, whitePawn)
 
@@ -44,4 +59,18 @@ class WhitePawnForwardMoveRuleTest extends AnyFunSuite {
     assert(!pawnRules.checkIfRestrictionIsMet(movementRestriction))
   }
 
+  val movedBoardCoordinate2: BoardCoordinate = BoardCoordinate(3, 3)
+  val movedCoordinateBoard2: SinglePieceBoard = SinglePieceBoard(movedBoardCoordinate2, whitePawn)
+
+  test("Single move forward test moved position redundancy") {
+    val movement = Movement(movedBoardCoordinate2, BoardCoordinate(3, 4))
+    val movementRestriction = MoveRestrictionInput(movement, movedCoordinateBoard2, List())
+    assert(pawnRules.checkIfRestrictionIsMet(movementRestriction))
+  }
+
+  test("Double move forward test moved position redundancy") {
+    val movement = Movement(movedBoardCoordinate2, BoardCoordinate(3, 5))
+    val movementRestriction = MoveRestrictionInput(movement, movedCoordinateBoard2, List())
+    assert(!pawnRules.checkIfRestrictionIsMet(movementRestriction))
+  }
 }
