609910fe8b28f4e878cbc8fdbe9e8eaa3af1764f
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-10-11 19:01:00 -0300

    Two move forward color dependant

--- chess_code/src/main/scala/movement_validation/movement_restriction/PawnDoubleStraightRule.scala
+++ chess_code/src/main/scala/movement_validation/movement_restriction/PawnDoubleStraightRule.scala
@@ -2,11 +2,13 @@ package movement_validation.movement_restriction:
   
   import board.BoardCoordinate
   import movement_validation.MoveRestrictionInput
+  import piece.PieceColor
   
     case class PawnDoubleStraightRule() extends MovementRestriction:
       def checkIfRestrictionIsMet(moveRestrictionInput: MoveRestrictionInput): Boolean =
         val from = moveRestrictionInput.movement.from
         val to = moveRestrictionInput.movement.to
         val vector = to.vector(from)
-        val piece = moveRestrictionInput.board.getPieceInBoardCoordinate(BoardCoordinate(to.x, to.y +1))
-        (vector == BoardCoordinate(0, 2) && from.y == 2) || (vector == BoardCoordinate(0, -2) && from.y == 7) && piece.isEmpty
+        val pieceInMiddle = moveRestrictionInput.board.getPieceInBoardCoordinate(BoardCoordinate(to.x, to.y +1))
+        val piece = moveRestrictionInput.board.getPieceInBoardCoordinate(from).get
+        (vector == BoardCoordinate(0, 2) && from.y == 2 && piece.color == PieceColor("WHITE")) || (vector == BoardCoordinate(0, -2) && from.y == 7 && piece.color == PieceColor("BLACK")) && pieceInMiddle.isEmpty
